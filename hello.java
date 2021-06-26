75ec0c4adde19886dbaae79c14c4769e6b42ce61
ip2india <ip2india@gmail.com>
2021-06-26 11:00:24 +0000

    Added Comment Lines for hello.java

--- hello.java
+++ hello.java
@@ -1,3 +1,4 @@
+// This is sample java programme which will print hello world on the screen
 class HelloWorld {
     public static void main( String []args ) {
         System.out.println( "Hello World!" );
